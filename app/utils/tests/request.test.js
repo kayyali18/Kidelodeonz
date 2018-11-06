dcc2386b5e7d4bd929a7d9de729db7b4951838e6
Ahmad Kayyali <kayyali18@gmail.com>
2018-11-05 19:11:10 -0700

    Test a no content error on the request helper

--- app/utils/tests/request.test.js
+++ app/utils/tests/request.test.js
@@ -37,4 +37,25 @@ describe('request', () => {
         })
     })
   })
+
+  describe('stubbing 204 response', () => {
+    // Before each test, pretend we got a succesfful response
+    beforeEach(() => {
+      const res = new Response('', {
+        status: 204,
+        statusText: 'No Content',
+      })
+
+      window.fetch.mockReturnValue(Promise.resolve(res))
+    })
+
+    it('should return null on 204 response', done => {
+      request('thisUrlIsCorrect')
+        .catch(done)
+        .then(json => {
+          expect(json).toBeNull()
+          done()
+        })
+    })
+  })
 })
